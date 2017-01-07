43dfc7f1083cb4d536603c5bed18eea4b33152bd
Pegtom <pegah.khorasani97@gmail.com>
2017-01-07 17:00:06 +0330

    shuru e bazi
    fln tabe ye start dare tu file controller ezafe mishe

---
+++ controller1.c
@@ -0,0 +1,16 @@
+/*
+* Author : Pegah Khorasani
+* Description: 
+* Functions::
+*		
+*/
+#include "dataStruct.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "database.h"
+#include "controller1.h"
+#include "utilies.h"
+
+void start(){
+	welcomeFootball();		
+}
--- main.c
+++ main.c
@@ -10,15 +10,9 @@
 #include "database.h"
 #include "profile.h"
 #include "utilities.h"
+#include "controller1.h"
+
 
 int main(){
-	//welcomeFootball();
-	team_t teams[16];
-	getTeamsDatabase(teams);
-	player_t *players;
-	int count = getPlayersDatabase(&players);
-	printAllPlayers(players, count, teams);
-	printTeamPlayers(players, count, teams, 3);
-	printAllTeamsList(teams, 16);
 	return (EXIT_SUCCESS);
 }
