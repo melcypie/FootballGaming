de38903b8ccceeaae4032f69be39e04765eddc14
Mahdi Alikhasi <mahdialikhasi1389@gmail.com>
2017-01-13 10:57:51 +0330

    Bug fix
    viod ==> void

--- controller2.h
+++ controller2.h
@@ -1,4 +1,4 @@
 void creatGamelist(void);
 void teamsSwap(team_t *t1, team_t *t2);
 void goalAssign(player_t *players, int index[2][11], int size);
-viod playWeek(int n, int *players, int size, int *teams);
+void playWeek(int n, int *players, int size, int *teams);
