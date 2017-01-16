81eb15910deae43afe758e6343f9e3ff3af63d02
leilynourbakhsh <leily.nourbakhsh36@gmail.com>
2017-01-16 07:40:16 -0800

    complete

--- controller2.h
+++ controller2.h
@@ -1,5 +1,5 @@
 void creatGamelist(void);
-void goalAssign(player_t *players, int index[2][11], int size);
+void goalAssign(player_t *players, int index[2][11], int size,int type);
 void playWeek(int n, player_t *players, int size, team_t *teams);
 void status(void);
 void lineup(void);
